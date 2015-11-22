dc4b35554b166539b960deac9d2e33e8be3018b6
loeppky <stvn@loeppky.com>
2015-11-22 01:30:56 -0800

    Commiting changes to contestant to enable it to be authenticated, and to pass through the authentication variables from the context.
    We had to util.escapeJavaScript.
    
    See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html#context-variable-reference

--- aws_modules/backend/contestant/index.js
+++ aws_modules/backend/contestant/index.js
@@ -4,16 +4,10 @@
 
 // Export For Lambda Handler
 module.exports.run = function(event, context, cb) {
-    return cb(null, action(event.seasonId, event.id, event.ids));
+    return cb(null, action(event));
 };
 
 // Your Code
-var action = function(seasonId, id, ids) {
-    if (ids) {
-        return { message : 'You requested the following contestants (seasonId: ' + seasonId + '): ' + ids.split(',') }
-    }
-    if (id) {
-        return { message : 'You requested the following contestant (seasonId: ' + seasonId + '): ' + id }
-    }
-    return { message : 'You requested all the contestants (seasonId: ' + seasonId + ')' };
+var action = function(event) {
+   return event;
 };
