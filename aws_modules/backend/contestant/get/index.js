7494b02a9fda44f824e6cf2c5a4cf285b2c1eb1c
Mitchell Loeppky <mitchell@loeppky.com>
2015-11-23 20:54:01 -0800

    Created contestants lambda

--- aws_modules/backend/contestant/get/index.js
+++ aws_modules/backend/contestant/get/index.js
@@ -2,12 +2,44 @@
  * AWS Module: Action: Modularized Code
  */
 
+var _ = require('lodash');
+var AWS = require('aws-sdk');
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+
 // Export For Lambda Handler
-module.exports.run = function(event, context, cb) {
-    return cb(null, action(event));
+module.exports.run = function(event, context, done) {
+    action(event.seasonId, event.id, event.ids, done);
 };
 
 // Your Code
-var action = function(event) {
-   return event;
+var action = function(seasonId, id, ids, done) {
+    if (!id && !ids) {
+        return dynamodbDoc.query({
+            TableName : process.env.CONTESTANTS_TABLE,
+            IndexName: 'seasonId-id-index',
+            KeyConditionExpression: 'seasonId = :seasonId',
+            ExpressionAttributeValues: {
+                ':seasonId': seasonId
+            }
+        }, function(err, data) {
+            if (err) { return done(err); }
+            done(null, data.Items);
+        });
+    }
+    ids = ids ? ids.split(',') : [id];
+
+    var params = {
+        RequestItems : {}
+    };
+    params.RequestItems[process.env.CONTESTANTS_TABLE] = {
+        Keys : _.map(ids, function(id) {
+            return {
+                id : id
+            }
+        })
+    };
+    return dynamodbDoc.batchGet(params, function(err, data) {
+        if (err) { return done(err); }
+        done(null, data.Responses[process.env.CONTESTANTS_TABLE]);
+    })
 };
